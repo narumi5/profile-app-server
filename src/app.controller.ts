4c0adedabc40920e8d093c160dd8d00ca979fd3c
森井成美 <narumi@moriinaruminoMacBook-Air.local>
2025-03-08 01:26:37 +0900

    hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller("hello")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -9,4 +9,15 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-}
+  @Get("2")
+  getHello2(): string {
+    return this.appService.getHello();
+  }
+  @Get("3")
+  getHello3(): string {
+    return this.appService.getHello3();
+  }
+  @Get("4")
+  getHello4(): string {
+    return this.appService.getHello4();
+  }}
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello Wor!';
   }
+  getHello2(): string {
+    return 'Hello World2!';
+  }
+  getHello3(): string {
+    return 'Hello Takuya!';
+  }
+  getHello4(): string {
+    let name = 'hello Takuya';
+    name = 'narumi';
+    return name;
+  }
 }
